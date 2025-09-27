42d8adb3e778c8ed4ad0df0a29633bdea3599f58
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-09-27 05:42:38 +0200

    Initial commit

---
+++ src/main/java/com/spring_security_keycloack/SpringSecurityKeycloackApplication.java
@@ -0,0 +1,13 @@
+package com.spring_security_keycloack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityKeycloackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityKeycloackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring_security_keycloack/SpringSecurityKeycloackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_security_keycloack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityKeycloackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
